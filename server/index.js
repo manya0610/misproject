704c59f740bfaf33ab231f82a38b1dc446cef0cb
manya0610 <manuaka.mindslicer@gmail.com>
2022-03-05 17:44:12 +0530

    fixed

---
+++ server/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const path = require("path");
+dotenv.config();
+
+/* Port setup*/
+const PORT = process.env.PORT || 5000;
+
+/* Cors Setup */
+app.use(cors());
+app.use(express.json());
+
+/* import routes*/
+const routehandler = require("./router");
+
+/* route middlewares */
+app.use("/api", routehandler);
+
+/* Serve static files in production */
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(__dirname + "/public"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+  );
+}
+
+/**Connect to mongoDB */
+mongoose.connect("mongodb://127.0.0.1:27017/test")
+	.then(() => {
+		console.log('Database connection successful')
+       })
+       .catch(err => {
+         console.error('Database connection error')
+       })
+
+/**Listen on Port */
+app.listen(PORT, () => console.log("server is started at port " + PORT));
